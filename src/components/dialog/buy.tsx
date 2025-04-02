0c1be5b84470aed68b3daaf744c0a14e20f39d37
pop <906792895@qq.com>
2025-04-02 16:17:13 +0800

    fix: buy confirm

--- src/components/dialog/buy.tsx
+++ src/components/dialog/buy.tsx
@@ -13,7 +13,7 @@ const DialogBuy = ({ trigger }: iDialogBuy) => {
       <DialogTrigger>{trigger}</DialogTrigger>
       <DialogContent className="p-0 h-auto flex justify-center">
         <div className="bgBuy dw680 dh660 relative">
-          <div className="flex justify-center items-cente dpt28 dpb42 dmb80">
+          <div className="flex justify-center items-cente dpt40 dmb70">
             <img className="w-[1.1vw] h-auto" src="/img/svg/paw.svg" alt="" />
             <span className="dml20 dmr20 text-white dtext35 font-[800] leading-none">
               Buy Kitty Kibble
@@ -31,9 +31,11 @@ const DialogBuy = ({ trigger }: iDialogBuy) => {
             Are you sure you want to purchase this item?
           </div>
           <div className="flex justify-center items-center">
-            <div className="flex cursor-pointer select-none dw266 dh80 buyButtonImg flex justify-center items-center">
-              Confirm
-            </div>
+            <DialogTrigger>
+              <div className="bgConfirmBtn cursor-pointer select-none dw266 dh80  flex justify-center items-center">
+                {/* Confirm */}
+              </div>
+            </DialogTrigger>
           </div>
           <div className="flex absolute top-0 right-0 translate-x-[100%] translate-y-[-80%]">
             <DialogTrigger>
