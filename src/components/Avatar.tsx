8e85478569fad1ea3f1da45a569b8e49b7ba19b2
NBSeven <410805964@qq.com>
2025-04-05 21:56:16 +0800

    feat: 点击头像全屏

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -4,8 +4,15 @@ import React from "react";
 import styles from "@/styles/Avatar.module.css"; // 确保有对应的 CSS 文件
 
 export default function Avatar(props: { nickname: string; avatar: string }) {
+  const enterFullscreen = () => {
+    if (typeof window === "undefined") return;
+    const elem = document.documentElement; // 选择整个网页
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    }
+  };
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={enterFullscreen}>
       <img className={styles.avatar} src={props.avatar} />
       <div className={styles.name}>{props.nickname}</div>
     </div>
