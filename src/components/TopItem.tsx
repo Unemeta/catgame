b0e75287d3cdfd7aea3e57bca726406b848c23a0
NBSeven <410805964@qq.com>
2025-04-08 15:28:57 +0800

    feat: 新增数字滚动

--- src/components/TopItem.tsx
+++ src/components/TopItem.tsx
@@ -1,14 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 import styles from "@/styles/TopItem.module.css"; // 确保有对应的 CSS 文件
+import CountUp from "react-countup";
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function TopItem({imgUrl,number,label}:any) {
+export default function TopItem({ imgUrl, number, label }: any) {
   return (
     <div className={styles.itemContainer}>
-      {/* <img src="./img/love.svg" alt="" className={styles.itemImage} /> */}
       <img src={imgUrl} alt="" className={styles.itemImage} />
       <div>
-        <div className={styles.itemNumber}>{number}</div>
+        <CountUp end={number} decimal="," className={styles.itemNumber} />
         <div className={styles.itemLabel}>{label}</div>
       </div>
     </div>
