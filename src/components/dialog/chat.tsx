8fdf7423dd22c5113b96933b387ba7855140b485
pop <906792895@qq.com>
2025-04-21 13:41:25 +0800

    fix: chat layout

--- src/components/dialog/chat.tsx
+++ src/components/dialog/chat.tsx
@@ -22,9 +22,9 @@ const DialogChatView = ({ trigger }: iDialogChatView) => {
     //     Authorization: `Bearer ${jwtHelper.getToken()}`,
     //   },
     // });
-    // socket.addEventListener("open", (event) => {
-    //   socket.send("Authorization: Bearer " + jwtHelper.getToken());
-    // });
+    socket.addEventListener("open", (event) => {
+      socket.send("Authorization: Bearer " + jwtHelper.getToken());
+    });
     socket.onopen = () => {
       console.log("Connected to the server");
       socket.send("Authorization: Bearer " + jwtHelper.getToken());
@@ -86,72 +86,74 @@ const DialogChatView = ({ trigger }: iDialogChatView) => {
               <span className="text-white dtext26 font-[500]">Back</span>
             </div>
           </div>
-          <div className="flex justify-end items-start dmb40">
-            <div className="chatCtt grid grid-cols-1 dgap20 dh730 overflow-y-scroll dpr36">
-              {[1, 1, 1, 1, 1, 1]?.map((item, index: number) => {
-                if (index % 2 === 0) {
-                  return (
-                    <div
-                      key={index}
-                      className="receive dp24 drounded30 bg-[rgba(90,_35,_92,_0.30)] flex justify-start items-start"
-                    >
-                      <img
-                        className="dw65 dh65 rounded-full dmr25"
-                        src="/img/1.jpg"
-                        alt=""
-                      />
-                      <div className="dmaxW460 lmd:max-w-[400px]">
-                        <div className="dtext24 font-[500] text-[#F5F2FF]/60 dmb8">
-                          2024/1/23 23:34
-                        </div>
-                        <div className="dtext28 font-[500] text-[#F5F2FF]">
-                          At least 34 people have been killed and 117 injured in
-                          a Russian attack on the city of Sumy, according to
-                          Ukrainian authorities.
-                        </div>
-                      </div>
-                    </div>
-                  );
-                } else {
-                  return (
-                    <div
-                      key={index}
-                      className="send dp24 drounded30 bg-[rgba(90,_35,_92,_0.30)] flex justify-start items-start"
-                    >
-                      <div className="dmaxW460 lmd:max-w-[400px] dmr25">
-                        <div className="dtext24 font-[500] text-[#F5F2FF]/60 dmb8 text-right">
-                          2024/1/23 23:34
-                        </div>
-                        <div className="dtext28 font-[500] text-[#F5F2FF]">
-                          At least 34 people have been killed and 117 injured in
-                          a Russian attack on the city of Sumy, according to
-                          Ukrainian authorities.
+          <div className="flex flex-col chatCttH">
+            <div className="flex justify-end items-start dmb40 grow overflow-y-scroll">
+              <div className="chatCtt grid grid-cols-1 dgap20 overflow-y-scroll dpr36">
+                {[1, 1, 1, 1, 1, 1]?.map((item, index: number) => {
+                  if (index % 2 === 0) {
+                    return (
+                      <div
+                        key={index}
+                        className="receive dp24 drounded30 bg-[rgba(90,_35,_92,_0.30)] flex justify-start items-start"
+                      >
+                        <img
+                          className="dw65 dh65 rounded-full dmr25"
+                          src="/img/1.jpg"
+                          alt=""
+                        />
+                        <div className="dmaxW460 lmd:max-w-[400px]">
+                          <div className="dtext24 font-[500] text-[#F5F2FF]/60 dmb8">
+                            2024/1/23 23:34
+                          </div>
+                          <div className="dtext28 font-[500] text-[#F5F2FF]">
+                            At least 34 people have been killed and 117 injured
+                            in a Russian attack on the city of Sumy, according
+                            to Ukrainian authorities.
+                          </div>
                         </div>
                       </div>
-                      <img
-                        className="dw65 dh65 rounded-full "
-                        src="/img/1.jpg"
-                        alt=""
-                      />
-                    </div>
-                  );
-                }
-              })}
-            </div>
-          </div>
-          <div className="textInput flex justify-center items-center">
-            <div className="relative">
-              <input
-                className="drounded160 dborderW4 border-[#FFF] dw780 dh100 bg-white/70 dmr30  dpl15 dpr155 outline-none text-[#522192] dtest24 font-[800]"
-                type="text"
-                onChange={(e) => setMessage(e.target.value)}
-                onKeyDown={handleKeyDown}
-              />
-              <div className="absolute dright80 top-[50%] dtranslateYF50 bg-[linear-gradient(0deg,_#BE6FFF_0%,_#6C8AFF_100%)] drounded20 dpx30 dpy20 text-white dtext24 font-[800]">
-                Send
+                    );
+                  } else {
+                    return (
+                      <div
+                        key={index}
+                        className="send dp24 drounded30 bg-[rgba(90,_35,_92,_0.30)] flex justify-start items-start"
+                      >
+                        <div className="dmaxW460 lmd:max-w-[400px] dmr25">
+                          <div className="dtext24 font-[500] text-[#F5F2FF]/60 dmb8 text-right">
+                            2024/1/23 23:34
+                          </div>
+                          <div className="dtext28 font-[500] text-[#F5F2FF]">
+                            At least 34 people have been killed and 117 injured
+                            in a Russian attack on the city of Sumy, according
+                            to Ukrainian authorities.
+                          </div>
+                        </div>
+                        <img
+                          className="dw65 dh65 rounded-full "
+                          src="/img/1.jpg"
+                          alt=""
+                        />
+                      </div>
+                    );
+                  }
+                })}
               </div>
             </div>
-            <img className="dw120 dh120" src="/img/speak.min.png" alt="" />
+            <div className="textInput flex justify-center items-center h-[100px] dpb80">
+              <div className="relative">
+                <input
+                  className="drounded160 dborderW4 border-[#FFF] dw780 dh100 bg-white/70 dmr30  dpl15 dpr155 outline-none text-[#522192] dtest24 font-[800]"
+                  type="text"
+                  onChange={(e) => setMessage(e.target.value)}
+                  onKeyDown={handleKeyDown}
+                />
+                <div className="absolute dright80 top-[50%] dtranslateYF50 bg-[linear-gradient(0deg,_#BE6FFF_0%,_#6C8AFF_100%)] drounded20 dpx30 dpy20 text-white dtext24 font-[800]">
+                  Send
+                </div>
+              </div>
+              <img className="dw120 dh120" src="/img/speak.min.png" alt="" />
+            </div>
           </div>
         </div>
       </DialogContent>
