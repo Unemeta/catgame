494f9dbbaddaff1df18e77b35347e0d387c98091
pop <906792895@qq.com>
2025-04-21 17:28:25 +0800

    fix: chat

--- src/components/dialog/chat.tsx
+++ src/components/dialog/chat.tsx
@@ -5,51 +5,103 @@ import { ReactNode, useEffect, useState } from "react";
 import { Dialog, DialogContent, DialogTitle, DialogTrigger } from ".";
 import styles from "@/styles/Backpack.module.css";
 import { jwtHelper } from "@/utils/jwt";
+import { request } from "@/utils/request";
+import { useFetchUser } from "@/store";
 
 interface iDialogChatView {
   trigger?: ReactNode;
 }
+let timerHistory: NodeJS.Timeout | null | undefined = null;
 const DialogChatView = ({ trigger }: iDialogChatView) => {
   const [isOpen, setIsOpen] = useState(false);
-
+  const { userData } = useFetchUser();
   const [socket, setSocket] = useState<WebSocket | null>(null);
-  const [message, setMessage] = useState("");
+  const [inputMsg, setinputMsg] = useState("");
+  const [messageList, setmessageList] = useState<
+    { chatId: string; msg: string; role: string; time: number }[]
+  >([]);
+  useEffect(() => {
+    (() => {
+      if (timerHistory) {
+        clearTimeout(timerHistory);
+      }
+      timerHistory = setTimeout(async () => {
+        getHistory();
+      }, 3000);
+    })();
+  }, []);
 
   useEffect(() => {
-    const socket = new WebSocket(`${process.env.NEXT_PUBLIC_WS_URL}`);
-    // const socket = new WebSocket(`${process.env.NEXT_PUBLIC_WS_URL}`, {
-    //   headers: {
-    //     Authorization: `Bearer ${jwtHelper.getToken()}`,
-    //   },
-    // });
-    socket.addEventListener("open", (event) => {
-      socket.send("Authorization: Bearer " + jwtHelper.getToken());
-    });
-    socket.onopen = () => {
-      console.log("Connected to the server");
-      socket.send("Authorization: Bearer " + jwtHelper.getToken());
+    if (socket) {
+      return;
+    }
+    const socketTemp = new WebSocket(
+      `${process.env.NEXT_PUBLIC_WS_URL}?token=${jwtHelper.getToken()}`
+    );
+    socketTemp.onopen = () => {
+      console.log("Connected to the server22");
     };
-    socket.onmessage = (event) => {
+    socketTemp.onmessage = (event) => {
+      if (event?.type === "message") {
+        setmessageList([
+          ...messageList,
+          {
+            chatId: userData?.nickname,
+            msg: event?.data,
+            role: "cat",
+            time: Math.floor(new Date().getTime() / 1000),
+          },
+        ]);
+      }
       // setMessages((prevMessages) => [...prevMessages, event.data]);
     };
-    socket.onclose = () => {
+    socketTemp.onclose = () => {
       console.log("Disconnected from the server");
+      setSocket(null);
     };
-    setSocket(socket);
+    setSocket(socketTemp);
     return () => {
-      socket.close();
+      socketTemp.close();
+      setSocket(null);
     };
   }, []);
 
   const sendMessage = () => {
-    if (socket && message) {
-      socket?.send(message);
-      setMessage("");
+    if (socket) {
+      if (inputMsg?.length > 0) {
+        debugger;
+        setmessageList([
+          ...messageList,
+          {
+            chatId: userData?.nickname,
+            msg: inputMsg,
+            role: "user",
+            time: Math.floor(new Date().getTime() / 1000),
+          },
+        ]);
+        socket?.send(inputMsg);
+        setinputMsg("");
+      } else {
+        alert("Please input msg");
+      }
     }
   };
   const handleKeyDown = (event: any) => {
     if (event?.key === "Enter") {
       sendMessage();
+    }
+  };
+  const getHistory = async () => {
+    try {
+      const { data } = await request({
+        url: `/api/cat/v1/chat/history?page=1&pageSize=1000`,
+        method: "get",
+      });
+      if (data?.msgList) {
+        setmessageList([...data.msgList.reverse()]);
+      }
+    } catch (error) {
+      debugger;
     }
   };
   return (
@@ -88,13 +140,34 @@ const DialogChatView = ({ trigger }: iDialogChatView) => {
           </div>
           <div className="flex flex-col chatCttH">
             <div className="flex justify-end items-start dmb40 grow overflow-y-scroll">
-              <div className="chatCtt grid grid-cols-1 dgap20 overflow-y-scroll dpr36">
-                {[1, 1, 1, 1, 1, 1]?.map((item, index: number) => {
-                  if (index % 2 === 0) {
+              <div className="chatCtt flex flex-col dgap20 overflow-y-scroll dpr36">
+                {messageList?.map((item, index: number) => {
+                  if (item?.role === "user") {
                     return (
                       <div
                         key={index}
-                        className="receive dp24 drounded30 bg-[rgba(90,_35,_92,_0.30)] flex justify-start items-start"
+                        className="send dp24 drounded30 bg-[rgba(90,_35,_92,_0.30)] flex justify-between items-start overflow-hidden"
+                      >
+                        <div className="dmaxW460 lmd:max-w-[400px] dmr25 w-full">
+                          <div className="dtext24 font-[500] text-[#F5F2FF]/60 dmb8 text-right">
+                            2024/1/23 23:34
+                          </div>
+                          <div className="dtext28 font-[500] text-[#F5F2FF]  text-wrap">
+                            {item?.msg}
+                          </div>
+                        </div>
+                        <img
+                          className="dw65 dh65 rounded-full "
+                          src="/img/1.jpg"
+                          alt=""
+                        />
+                      </div>
+                    );
+                  } else {
+                    return (
+                      <div
+                        key={index}
+                        className="receive dp24 drounded30 bg-[rgba(90,_35,_92,_0.30)] flex justify-start items-start overflow-hidden"
                       >
                         <img
                           className="dw65 dh65 rounded-full dmr25"
@@ -105,35 +178,10 @@ const DialogChatView = ({ trigger }: iDialogChatView) => {
                           <div className="dtext24 font-[500] text-[#F5F2FF]/60 dmb8">
                             2024/1/23 23:34
                           </div>
-                          <div className="dtext28 font-[500] text-[#F5F2FF]">
-                            At least 34 people have been killed and 117 injured
-                            in a Russian attack on the city of Sumy, according
-                            to Ukrainian authorities.
+                          <div className="dtext28 font-[500] text-[#F5F2FF] text-wrap whitespace-normal">
+                            {item?.msg}
                           </div>
                         </div>
-                      </div>
-                    );
-                  } else {
-                    return (
-                      <div
-                        key={index}
-                        className="send dp24 drounded30 bg-[rgba(90,_35,_92,_0.30)] flex justify-start items-start"
-                      >
-                        <div className="dmaxW460 lmd:max-w-[400px] dmr25">
-                          <div className="dtext24 font-[500] text-[#F5F2FF]/60 dmb8 text-right">
-                            2024/1/23 23:34
-                          </div>
-                          <div className="dtext28 font-[500] text-[#F5F2FF]">
-                            At least 34 people have been killed and 117 injured
-                            in a Russian attack on the city of Sumy, according
-                            to Ukrainian authorities.
-                          </div>
-                        </div>
-                        <img
-                          className="dw65 dh65 rounded-full "
-                          src="/img/1.jpg"
-                          alt=""
-                        />
                       </div>
                     );
                   }
@@ -145,10 +193,15 @@ const DialogChatView = ({ trigger }: iDialogChatView) => {
                 <input
                   className="drounded160 dborderW4 border-[#FFF] dw780 dh100 bg-white/70 dmr30  dpl15 dpr155 outline-none text-[#522192] dtest24 font-[800]"
                   type="text"
-                  onChange={(e) => setMessage(e.target.value)}
+                  onChange={(e) => setinputMsg(e.target.value)}
                   onKeyDown={handleKeyDown}
+                  value={inputMsg}
                 />
-                <div className="absolute dright80 top-[50%] dtranslateYF50 bg-[linear-gradient(0deg,_#BE6FFF_0%,_#6C8AFF_100%)] drounded20 dpx30 dpy20 text-white dtext24 font-[800]">
+                <div
+                  className="absolute dright80 top-[50%] dtranslateYF50 bg-[linear-gradient(0deg,_#BE6FFF_0%,_#6C8AFF_100%)] drounded20 dpx30 dpy20 text-white dtext24 font-[800] cursor-pointer select-none"
+                  onClick={sendMessage}
+                  onKeyDown={handleKeyDown}
+                >
                   Send
                 </div>
               </div>
