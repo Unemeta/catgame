0a0ba351f5807a944ee39977cf5d5ccd73f3bd4f
pop <906792895@qq.com>
2025-04-24 08:46:48 +0800

    fix: play dialog css

--- src/components/dialog/play.tsx
+++ src/components/dialog/play.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @next/next/no-img-element */
 import { ReactNode, useEffect, useRef } from "react";
-import { Dialog, DialogContent, DialogTitle } from ".";
+import { Dialog, DialogContent, DialogTitle, DialogTrigger } from ".";
 import DialogTitleView from "./dialogTitle";
 
 const videoUrl = "/videos/playball.mp4";
@@ -46,7 +46,7 @@ const DialogPlay = ({ isOpen, playEnd, setIsOpen }: iDialogShop) => {
     }}>
       <DialogContent className="p-0 dw1066 h-auto max-w-[100vw]">
         <DialogTitle></DialogTitle>
-        <div className="dw1066 dh660 bgFeeding relative dpt100 dpl30 dpr30 dpb30">
+        <div className="dw1066 dh660 bgFeeding relative dpt85 dpl25 dpr25 dpb20">
           <DialogTitleView title="Play"></DialogTitleView>
           <video
             ref={refVideo}
@@ -55,8 +55,13 @@ const DialogPlay = ({ isOpen, playEnd, setIsOpen }: iDialogShop) => {
             playsInline
             src={videoUrl}
             preload="auto"
-            className="w-full h-full object-cover"
+            className="w-full h-full object-cover rounded-[10px]"
           ></video>
+            <DialogTrigger>
+            <div className="absolute dtop45 drightF10 flex justify-center items-center cursor-pointer select-none">
+              <img className="dw48 h-auto" src="/img/close.min.png" alt="" />
+            </div>
+          </DialogTrigger>
         </div>
       </DialogContent>
     </Dialog>
