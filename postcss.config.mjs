43294a2bf845425a45643bc95b2973354ca1f871
yf_pop <yf_pop@gmail.com>
2025-04-27 10:24:12 +0800

    fix: h5 medaiquery

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -10,7 +10,7 @@ const config = {
       propList: ["*", "!border*"], // ✅ 确保包含 width
       selectorBlackList: [".ignore", ".hairlines"], // 指定不需要转换的类
       minPixelValue: 1, // 小于或等于1px不转换为视口单位
-      mediaQuery: false, // 允许在媒体查询中转换px
+      mediaQuery: true, // 允许在媒体查询中转换px
     },
     autoprefixer: {},
   },
