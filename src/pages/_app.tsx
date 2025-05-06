ef83f309e55b1683ebc91973db30e12735ed7a49
pop <906792895@qq.com>
2025-05-06 16:52:23 +0800

    fix: user-scalable

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -42,7 +42,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, viewport-fit=cover"
+          content="width=device-width, initial-scale=1,user-scalable=no, viewport-fit=cover"
         />
       </Head>
       {isPortrait && false ? (
