69c8ebe2435b92d902cc0f54824a5c0bb21a5ffa
pop <906792895@qq.com>
2025-05-15 14:37:20 +0800

    fix: chat num

--- src/components/tooltip/chatNum.tsx
+++ src/components/tooltip/chatNum.tsx
@@ -1,6 +1,11 @@
+// import { useState } from "react";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from ".";
 
 const TooltipChatNum = () => {
+  // const [isOpen, setIsOpen] = useState(false);
+  // const handleToggle = () => {
+  //   setIsOpen((prev) => !prev);
+  // };
   return (
     <TooltipProvider>
       <Tooltip>
@@ -33,7 +38,11 @@ const TooltipChatNum = () => {
         <TooltipContent>
           {/* Remaining chats today. */}
           {/* <div className="chatNumTipText"></div> */}
-          <img className="chatNumTipText" src="/img/bg/tooltipChatInfo.min.png" alt="" />
+          <img
+            className="chatNumTipText"
+            src="/img/bg/tooltipChatInfo.min.png"
+            alt=""
+          />
         </TooltipContent>
       </Tooltip>
     </TooltipProvider>
