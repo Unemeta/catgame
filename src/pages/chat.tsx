bdd5c758b22227e526dce7ac89411497e609dfa9
pop <906792895@qq.com>
2025-05-27 12:34:55 +0800

    fix: delete console

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -63,7 +63,6 @@ const ChatView = () => {
     socketTemp.onmessage = (event) => {
       if (event?.type === "message" && event?.data !== "pong") {
         const msgRes = JSON.parse(event?.data);
-        console.log(msgRes);
         if (msgRes.hasOwnProperty("message") && msgRes["message"] != "Msg received") {
           //
         } else {
