c96c65ae7ca3255949a04ad01c3e7a6ac39455ac
pop <906792895@qq.com>
2025-06-13 20:22:51 +0800

    fix: party bg

---
+++ src/pages/party.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable @next/next/no-img-element */
+const PartyView = () => {
+  return (
+    <div className="">
+      <div className="relative">
+        <img
+          className="w-[100vw] h-[100vh] absolute inset-0 z-[1]"
+          src="/img/bg/bg_pop_rain.min.png"
+          alt=""
+        />
+        <div className="w-full h-full absolute inset-0 z-[2]">
+            
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PartyView;
