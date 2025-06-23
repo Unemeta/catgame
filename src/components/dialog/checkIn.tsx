8a1cfcaf7d02aa5fabe987a94f9e3bd42d021c23
NBSeven <410805964@qq.com>
2025-06-23 10:19:46 +0800

    feat: 签到联调完成

--- src/components/dialog/checkIn.tsx
+++ src/components/dialog/checkIn.tsx
@@ -1,42 +1,181 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
-import { ReactNode } from "react";
+import { useState, useEffect } from "react";
 import { Dialog, DialogContent, DialogTrigger, DialogTitle } from ".";
 import { useCheckInDia } from "@/store";
-interface iDialogRewards {
-  trigger?: ReactNode;
-  id: number;
-}
-const DialogCheckIn = ({ id }: iDialogRewards) => {
-  const [showCheckIn, setShowCheckIn] = useCheckInDia();
-  return (
-    <Dialog open={showCheckIn} onOpenChange={setShowCheckIn}>
-      <DialogTitle></DialogTitle>
-      <DialogContent>
-        <div className="w-[30rem] h-[35rem] relative">
-          <div className="self-stretch bg-gradient-to-b from-red-300 to-stone-500/0 rounded-[3rem] shadow-[0px_4px_20px_0px_rgba(0,0,0,0.55)] outline outline-2 outline-offset-[-2px] outline-stone-50 inline-flex flex-col justify-start items-center gap-4 overflow-hidden w-full">
-            {/* 标题 */}
-            <div
-              className="text-center justify-start text-yellow-900 text-[1.8rem] font-extrabold font-['SF_Pro_Rounded'] leading-snug p-[1rem] rounded-tl-[3rem] rounded-tr-[3rem] w-full"
-              style={{
-                background: "linear-gradient(180deg, #E3BFA5 0%, #DDB293 100%",
-              }}
-            >
-              Daily Check-in
-            </div>
+import { cn } from "@/lib/utils";
+import { request } from "@/utils/request";
 
-            <div className="self-stretch px-[2rem] flex flex-col justify-start items-center gap-2.5">
-              <div className="self-stretch text-white text-[1.2rem] leading-none [text-shadow:_0px_0px_8px_rgb(255_255_255_/_0.45)] text-center">
-                Please visit every day to get better rewards.
+const SignToast = () => {
+  return (
+    <div
+      className="w-[100%] px-[6rem] py-[1rem] outline-1 outline-offset-[-1px] outline-stone-400/0 inline-flex flex-col justify-center items-center gap-2 z-100 top-[40%]"
+      style={{
+        background:
+          "linear-gradient(90deg, rgba(108, 73, 55, 0.00) 0%, #6C4937 49.52%, rgba(108, 73, 55, 0.00) 100%)",
+      }}
+    >
+      <div className="relative opacity-90 overflow-hidden">
+        <img
+          src="/img/check-circle-broken.png"
+          alt=""
+          className="w-[1.8rem] h-[1.8rem]"
+        />
+      </div>
+      <div className="self-stretch text-center justify-start text-white text-[1.4rem] font-bold font-['SF_Pro_Rounded'] leading-none">
+        Sign in successfully
+      </div>
+    </div>
+  );
+};
+const DialogCheckIn = () => {
+  const [showCheckIn, setShowCheckIn] = useCheckInDia();
+  const [loading, setLoading] = useState(false);
+  const [checkdays, setCheckdays] = useState([
+    {
+      day: 1,
+      checked: false,
+    },
+    {
+      day: 2,
+      checked: false,
+    },
+    {
+      day: 3,
+      checked: false,
+    },
+    {
+      day: 4,
+      checked: false,
+    },
+    {
+      day: 5,
+      checked: false,
+    },
+    {
+      day: 6,
+      checked: false,
+    },
+    {
+      day: 7,
+      checked: false,
+    },
+  ]);
+  const [showToast, setShowToast] = useState(false);
+
+  const checkIn = async () => {
+    try {
+      setLoading(true);
+      const res = await request({
+        url: "/api/cat/v1/user/checkin",
+        method: "post",
+      });
+      console.log(res);
+      const { checkinNums, isCheckin } = res.data;
+      setCheckdays(
+        checkdays.map((item) => {
+          if (item.day <= checkinNums) {
+            return {
+              ...item,
+              checked: true,
+            };
+          }
+          return item;
+        })
+      );
+      if (isCheckin) {
+        setShowToast(true);
+        setTimeout(() => {
+          setShowToast(false);
+        }, 1000);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    setLoading(false);
+  };
+  useEffect(() => {
+    checkIn();
+  }, []);
+  return (
+    <>
+      <Dialog open={showCheckIn} onOpenChange={setShowCheckIn}>
+        <DialogTitle></DialogTitle>
+
+        <DialogContent>
+          {showToast && <SignToast></SignToast>}
+
+          <div className="w-[30rem] h-[35rem] relative">
+            <div className="self-stretch bg-gradient-to-b from-red-300 to-stone-500/0 rounded-[3rem] shadow-[0px_4px_20px_0px_rgba(0,0,0,0.55)] outline-2 outline-offset-[-2px] outline-stone-50 inline-flex flex-col justify-start items-center gap-4 overflow-hidden w-full">
+              {/* 标题 */}
+              <div
+                className="text-center justify-start text-yellow-900 text-[1.8rem] font-extrabold font-['SF_Pro_Rounded'] leading-snug p-[1rem] rounded-tl-[3rem] rounded-tr-[3rem] w-full"
+                style={{
+                  background:
+                    "linear-gradient(180deg, #E3BFA5 0%, #DDB293 100%",
+                }}
+              >
+                Daily Check-in
               </div>
-              <div className="self-stretch inline-flex justify-start items-start gap-1.5">
-                {/* 左边的猫图 */}
-                <img
-                  className="w-[9rem] self-stretch rounded-[10px]  outline-1 outline-stone-400/80"
-                  src="/img/catexample.png"
-                />
-                <div className="flex-1 flex justify-around items-start gap-1 flex-wrap">
-                  <div className="p-2 relative bg-white/10 rounded-[10px]  outline-1 outline-offset-[-1px] outline-white/30 inline-flex flex-col justify-start items-center gap-px w-[32%]">
+
+              <div className="self-stretch px-[2rem] flex flex-col justify-start items-center gap-2.5">
+                <div className="self-stretch text-white text-[1.2rem] leading-none [text-shadow:_0px_0px_8px_rgb(255_255_255_/_0.45)] text-center">
+                  Please visit every day to get better rewards.
+                </div>
+                <div className="self-stretch inline-flex justify-start items-start gap-1.5">
+                  {/* 左边的猫图 */}
+                  <img
+                    className="w-[9rem] self-stretch rounded-[10px]  outline-1 outline-stone-400/80"
+                    src="/img/catexample.png"
+                  />
+                  <div className="flex-1 flex justify-around items-start gap-[0.5rem] flex-wrap">
+                    {checkdays.map((item) => {
+                      return (
+                        <div
+                          className={cn(
+                            "p-[0.8rem] relative bg-white/30 rounded-[10px] outline-1 outline-offset-[-1px] outline-white/20 inline-flex flex-col justify-start items-center gap-px w-[31%]",
+                            {
+                              "w-[100%]": item.day === 7,
+                            }
+                          )}
+                          key={item.day}
+                          onClick={checkIn}
+                        >
+                          <div
+                            className={cn("relative w-[4rem]", {
+                              "opacity-50": item.checked,
+                            })}
+                          >
+                            <img className="" src="/img/fishitem.png" />
+                          </div>
+                          <div
+                            className={cn(
+                              "self-stretch text-center justify-start text-stone-600 text-[1.4rem] font-semibold font-['SF_Pro_Rounded'] leading-3 mt-[0.3rem]",
+                              {
+                                "opacity-50": item.checked,
+                              }
+                            )}
+                          >
+                            x20
+                          </div>
+                          {item.checked ? (
+                            <img
+                              src="/img/footcheck.png"
+                              alt=""
+                              className="w-[2.7rem] h-[2.7rem] left-[-0.4rem] top-[-0.4rem] absolute"
+                            />
+                          ) : (
+                            <div className="px-[0.5rem] py-[0.3rem] left-[-0.4rem] top-[-0.4rem] absolute bg-white rounded-[2rem] outline-1 outline-white inline-flex justify-center items-center gap-2.5">
+                              <div className="justify-start text-red-400 text-[1.2rem] font-['SF_Pro_Rounded'] leading-3">
+                                Day {item.day}
+                              </div>
+                            </div>
+                          )}
+                        </div>
+                      );
+                    })}
+                    {/* <div className="p-2 relative bg-white/10 rounded-[10px]  outline-1 outline-offset-[-1px] outline-white/30 inline-flex flex-col justify-start items-center gap-px w-[32%]">
                     <div className="relative opacity-50 w-[4rem]">
                       <img className="" src="/img/fishitem.png" />
                     </div>
@@ -87,34 +226,53 @@ const DialogCheckIn = ({ id }: iDialogRewards) => {
                         Day 4
                       </div>
                     </div>
+                  </div> */}
                   </div>
                 </div>
               </div>
-            </div>
 
-            <div className="flex justify-center items-center">
-              <DialogTrigger>
-                <div className="self-stretch pb-4 flex flex-col justify-start items-center gap-2.5">
-                  <div className="w-[13.2rem] h-[3.8rem] px-5 py-3 bg-white rounded-[31.74px] shadow-[0px_4px_24px_0px_rgba(214,90,63,0.60)] shadow-[0px_3px_14px_0px_rgba(255,255,255,0.45)] inline-flex justify-center items-center gap-2">
-                    <div className="text-center justify-start text-red-400 text-[1.6rem] font-extrabold font-['SF_Pro_Rounded'] leading-tight">
-                      Confirm
+              <div className="flex justify-center items-center">
+                <DialogTrigger>
+                  <div
+                    className={cn(
+                      "self-stretch pb-4 flex flex-col justify-start items-center gap-2.5",
+                      {
+                        "opacity-50": loading,
+                      }
+                    )}
+                  >
+                    <div className="w-[13.2rem] h-[3.8rem] px-5 py-3 bg-white rounded-[31.74px] shadow-[0px_4px_24px_0px_rgba(214,90,63,0.60)] shadow-[0px_3px_14px_0px_rgba(255,255,255,0.45)] inline-flex justify-center items-center gap-2">
+                      {loading && (
+                        <img
+                          src="/img/loading.png"
+                          alt=""
+                          className="w-[1.8rem]"
+                        />
+                      )}
+                      <div className="text-center justify-start text-[#E96856] text-[1.6rem] font-extrabold font-['SF_Pro_Rounded'] leading-tight">
+                        Confirm
+                      </div>
                     </div>
                   </div>
+                </DialogTrigger>
+              </div>
+            </div>
+
+            <div className="flex absolute top-[2rem] right-[2.5rem] translate-x-[100%] translate-y-[-80%]">
+              <DialogTrigger>
+                <div className="flex justify-center items-center cursor-pointer select-none">
+                  <img
+                    className="w-[4rem] h-auto"
+                    src="/img/close.png"
+                    alt=""
+                  />
                 </div>
               </DialogTrigger>
             </div>
           </div>
-
-          <div className="flex absolute top-[2rem] right-[2.5rem] translate-x-[100%] translate-y-[-80%]">
-            <DialogTrigger>
-              <div className="flex justify-center items-center cursor-pointer select-none">
-                <img className="w-[4rem] h-auto" src="/img/close.png" alt="" />
-              </div>
-            </DialogTrigger>
-          </div>
-        </div>
-      </DialogContent>
-    </Dialog>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
