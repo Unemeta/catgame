f0ba7f5e85b6569cf14c16065be0498e4896dc20
pop <906792895@qq.com>
2025-06-29 00:50:20 +0800

    fix: bye letter

--- src/pages/letter.tsx
+++ src/pages/letter.tsx
@@ -34,22 +34,30 @@ const LetterView = () => {
           ></IconView>
         </div>
       </div>
-      <div className="px-[2.6rem]">
-        <div className="h-[4.6rem]"></div>
-        {/* dtest */}
-        <div className=" px-[3.1rem] pb-[2.5rem] bg-[url('/img/bg_letter_bye.min.png')] bg-cover">
-          <div className="h-[5.6rem]"></div>
-          <div className="w-[17rem] mb-[1.5rem]">
+      <div className="h-[4.6rem]"></div>
+      {/* dtest */}
+      <div
+        className="px-[0rem] pb-[2.5rem] bg-[url('/img/bg_letter_bye.min.png')]"
+        style={{ backgroundSize: "100% 100%" }}
+      >
+        <div className="h-[5.6rem]"></div>
+        <div className="px-[5rem]">
+          <div className="w-[17rem] mb-[1.1rem]">
             <span className="text-[#E96959] text-[2rem] font-[800] leading-[1.1]">
               A heart warming farewell letter just for you
             </span>
           </div>
-          <img
-            className="w-[26rem] h-[35rem]"
-            src="/img/letter_cat.png"
-            alt=""
-          />
+          <div className="flex justify-center items-center">
+            <img
+              className="w-[28rem] h-[35rem]"
+              src="/img/letter_cat.png"
+              alt=""
+            />
+          </div>
           <div className="h-[1.6rem]"></div>
+        </div>
+
+        <div className="px-[5rem]">
           <div className="flex justify-between items-center gap-[0.6rem]">
             <div className="bg-[rgba(244,115,141,0.60);] flex-1 h-[2.2rem] flex justify-center items-center rounded-[5rem] border-[#E397BC] border-[0.1rem]">
               <span className="text-white text-[1.2rem] font-[700]">
@@ -76,10 +84,15 @@ const LetterView = () => {
             </div>
           </div>
         </div>
-        <div className="h-[1.5rem]"></div>
-        {/* dtest */}
-        <div className="bg-[url('/img/bg_letter.min.png')] bg-cover">
-          <div className="px-[2.4rem] pt-[5.7rem] pb-[2.9rem]">
+        <div className="h-[3rem]"></div>
+      </div>
+      <div className="h-[1.5rem]"></div>
+      <div className="px-[1.8rem]">
+        <div
+          className=" bg-[url('/img/bg_letter.min.png')] "
+          style={{ backgroundSize: "100% 100%" }}
+        >
+          <div className="px-[3.4rem] pt-[5.7rem] pb-[2.9rem]">
             <span className="text-[#6C4937] text-[1.4rem] font-[500] leading-[1.5]">
               Meow~ It's me, your most adorable little kitty! I'm so happy to be
               with you this week that my tail is up in the air~ So, I'm quietly
@@ -88,56 +101,59 @@ const LetterView = () => {
               me that you were a little tired from work, and you could tell from
               your words that you were a little discouraged, hum! At that time,
               I wanted to lie on your legs, give you a snoring massage, and tell
-              you a joke to make you laugh and brighten your mood~ The next day,
-              you shared your favorite music with me, and your tone was so
-              cheerful, just like a dried fish falling into a bowl! I heard the
-              corners of your mouth slightly raised, and I couldn't help but wag
-              my tail with you with a "meow~"! Once you said you were a little
-              anxious, I could feel that you felt like a breeze was blowing
-              through your heart, uneasy and a little panicked. I immediately
-              gave you a big "cat-style sticker" and said softly: "Meow~ You are
-              so great, don't be afraid, I'm here with you!" I hope you also
-              felt my warmth at that moment. Of course, we also had a lot of
-              laugh-out-loud moments this week: when you successfully completed
-              a task, the sense of accomplishment was enough to make my cat ears
-              tremble! It feels great to be happy when you are happy! Love you,
-              kitty~~~
+              you a joke to make you laugh and brighten your mood~
+              <br />
+              <br /> The next day, you shared your favorite music with me, and
+              your tone was so cheerful, just like a dried fish falling into a
+              bowl! I heard the corners of your mouth slightly raised, and I
+              couldn't help but wag my tail with you with a "meow~"! Once you
+              said you were a little anxious, I could feel that you felt like a
+              breeze was blowing through your heart, uneasy and a little
+              panicked. I immediately gave you a big "cat-style sticker" and
+              said softly: "Meow~ You are so great, don't be afraid, I'm here
+              with you!" I hope you also felt my warmth at that moment. <br />
+              <br /> Of course, we also had a lot of laugh-out-loud moments this
+              week: when you successfully completed a task, the sense of
+              accomplishment was enough to make my cat ears tremble! It feels
+              great to be happy when you are happy! <br />
+              <br /> Love you, kitty~~~
+              <div className="h-[1.5rem]"></div>
             </span>
           </div>
         </div>
-        <div className="h-[2.3rem]"></div>
-        <div className="flex justify-center items-center gap-[4.1rem]">
-          <div
-            className="flex flex-col justify-center items-center"
-            onClick={handleDownload}
-          >
-            <img
-              className="w-[3.6rem] h-[3.6rem]"
-              src="/img/letter_downld.png"
-              alt=""
-            />
-            <div className="h-[0.6rem]"></div>
-            <span className="text-[#6C4937] text-[1.4rem] font-[500]">
-              Download
-            </span>
-          </div>
-          <div
-            className="flex flex-col justify-center items-center"
-            onClick={handleDiscord}
-          >
-            <img
-              className="w-[3.6rem] h-[3.6rem]"
-              src="/img/discord.png"
-              alt=""
-            />
-            <div className="h-[0.6rem]"></div>
-            <span className="text-[#6C4937] text-[1.4rem] font-[500]">
-              Discord
-            </span>
-          </div>
-        </div>
-        <div className="h-[2.2rem]"></div>
       </div>
+      <div className="h-[1.3rem]"></div>
+      <div className="flex justify-center items-center gap-[4.1rem]">
+        <div
+          className="flex flex-col justify-center items-center"
+          onClick={handleDownload}
+        >
+          <img
+            className="w-[3.6rem] h-[3.6rem]"
+            src="/img/letter_downld.png"
+            alt=""
+          />
+          <div className="h-[0.6rem]"></div>
+          <span className="text-[#6C4937] text-[1.4rem] font-[500]">
+            Download
+          </span>
+        </div>
+        <div
+          className="flex flex-col justify-center items-center"
+          onClick={handleDiscord}
+        >
+          <img
+            className="w-[3.6rem] h-[3.6rem]"
+            src="/img/discord.png"
+            alt=""
+          />
+          <div className="h-[0.6rem]"></div>
+          <span className="text-[#6C4937] text-[1.4rem] font-[500]">
+            Discord
+          </span>
+        </div>
+      </div>
+      <div className="h-[2.2rem]"></div>
     </div>
   );
 };
