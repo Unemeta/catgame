5acc3be0193bffbe2a1d6ad0312f68c3ff8d6edc
pop <906792895@qq.com>
2025-07-10 20:07:57 +0800

    fix: update letter dialog

--- src/components/Discord.tsx
+++ src/components/Discord.tsx
@@ -1,6 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 
-export default function Discord() {
+import { cn } from "@/lib/utils";
+
+interface iDiscord {
+  text?: string;
+  textColor?: string;
+}
+export default function Discord({ text, textColor }: iDiscord) {
   const handleDiscord = async () => {
     window.open("https://discord.com/invite/YzztkC6ENe", "_blank");
   };
@@ -11,7 +17,14 @@ export default function Discord() {
     >
       <img className="w-[3.6rem] h-[3.6rem]" src="/img/discord.png" alt="" />
       <div className="h-[0.6rem]"></div>
-      <span className="text-[#6C4937] text-[1.4rem] font-[500]">Discord</span>
+      <span
+        className={cn(
+          "text-[1.4rem] font-[500]",
+          textColor ?? "text-[#6C4937]"
+        )}
+      >
+        {text ?? "Discord"}
+      </span>
     </div>
   );
 }
