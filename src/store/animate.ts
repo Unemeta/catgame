8aab7d75f5653eee89a67e5658edac6f2ee3cde8
NBSeven <410805964@qq.com>
2025-07-12 22:18:27 +0800

    fix: 签到自动显示

--- src/store/animate.ts
+++ src/store/animate.ts
@@ -68,10 +68,10 @@ export const useShowVocie = () => {
 };
 
 
-const showCheckInDia = atom<boolean>(false) // 是否显示签到弹框
+const showCheckInDia = atom<boolean>(true) // 是否显示签到弹框
 
 export const useCheckInDia = () => {
-    useHydrateAtoms([[showCheckInDia, false]] as const);
+    useHydrateAtoms([[showCheckInDia, true]] as const);
     return useAtom(showCheckInDia);
 };
 
