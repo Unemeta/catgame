2208f9f3e753b0a0073d5de2ebe8cfac15b96a2c
pop <906792895@qq.com>
2025-07-16 22:07:54 +0800

    fix：cat avatar

--- src/components/dialog/avatar_cat.tsx
+++ src/components/dialog/avatar_cat.tsx
@@ -58,39 +58,12 @@ const DialogAvatarCat = ({ trigger, indexXingGe, catName }: iDialogSetting) => {
         //     "linear-gradient(187deg, #EDA88D -9.74%, rgba(157, 137, 87, 0.00) 72.93%, rgba(240, 233, 225, 0.60));",
         // }}
         >
-          <div className="px-[0rem]">
+          {/* <div className="px-[0rem]">
             <div
               className="px-[1.6rem] py-[1.6rem] relative h-[55vh] bg-[url('/img/bg/bg_card_cat.min.png')]"
               style={{ backgroundSize: "100% 100%" }}
             >
-              {indexXingGe != null && (
-                <div className="">
-                  <div className="px-[1rem] py-2 text-white text-[1.6rem] font-[700] bg-[#F4738D99] rounded-[5rem] border-[#E397BC] border-w-[0.1rem] inline-block">
-                    {cats[indexXingGe].keys[0]}
-                  </div>
-                </div>
-              )}
-              {indexXingGe != null && (
-                <div className="">
-                  <div className="px-[1rem] py-2 text-white text-[1.6rem] font-[700] bg-[#2278C199] rounded-[5rem] border-[#6D9FDA] border-w-[0.1rem] inline-block my-[0.6rem]">
-                    {cats[indexXingGe].keys[1]}
-                  </div>
-                </div>
-              )}
-              {indexXingGe != null && cats[indexXingGe].keys.length > 2 && (
-                <div className="">
-                  <div className="px-[1rem] py-2 text-white text-[1.6rem] font-[700] bg-[#4A5F7799] rounded-[5rem] border-[#95A1C1] border-w-[0.1rem] inline-block">
-                    {cats[indexXingGe].keys[2]}
-                  </div>
-                </div>
-              )}
-              {indexXingGe != null && (
-                <div className="absolute right-[1.6rem] top-[1.6rem] bg-[#B4886D] border-[1px] border-[#DBB399] rounded-[5rem] flex justify-center items-center px-[1rem] h-[2.8rem]">
-                  <span className="text-white text-[1.2rem] font-[700] text-center leading-[1]">
-                    {cats[indexXingGe].type}
-                  </span>
-                </div>
-              )}
+              
               <div className="absolute left-[0] top-[4rem] w-full flex justify-center">
                 <img
                   src="/img/bg_cat.min.png"
@@ -98,24 +71,16 @@ const DialogAvatarCat = ({ trigger, indexXingGe, catName }: iDialogSetting) => {
                   alt=""
                 />
               </div>
-              <div className="absolute bottom-0 left-0 w-full h-[40%]">
-                <div className="flex justify-center items-center">
-                  {/* <div className="bg-[url('/img/bg/bg_aloof_cat.min.png')] bg-cover bg-no-repeat text-white text-[2.8rem] font-[700] h-[5.3rem] px-[5rem] flex justify-center items-center">
-                {cats[index].type}
-              </div> */}
+              <div className="absolute bottom-0 left-0 w-full h-[40%] dtest bg-white rounded-bl-[3rem] rounded-br-[3rem]">
+                <div className="flex justify-center items-center dtest translate-y-[-50%]">
                   <div className="relative">
                     <input
                       tabIndex={-1}
                       readOnly
-                      // onChange={(e) => setinputMsg(e.target.value)}
                       value={catName}
                       className="border-[1px] focus:outline-none! border-[#E96856] rounded-full bg-white w-[21rem] h-[4rem] text-[#EA8273] text-[2rem] font-[700] px-[2rem] pr-[3.6rem] text-center"
                       type="text"
                     />
-                    {/* <IconView
-                      className="w-[1.5rem] h-[1.5rem] absolute right-[1.8rem] top-[1.2rem]"
-                      type="eduitCatName"
-                    ></IconView> */}
                   </div>
                 </div>
                 {indexXingGe != null && (
@@ -125,10 +90,76 @@ const DialogAvatarCat = ({ trigger, indexXingGe, catName }: iDialogSetting) => {
                 )}
               </div>
             </div>
+          </div> */}
+          <div className="px-[3rem]">
+            <div
+              className="pt-[1.6rem] relative bg-[url('/img/bg/bg_card_cat.min.png')]"
+              style={{ backgroundSize: "100% 100%" }}
+            >
+              <div className="px-[1.6rem] bg-[url('/img/bg/bg_card_cat_top.min.png') h-[26rem] relative">
+                {indexXingGe != null && (
+                  <div className="">
+                    <div className="px-[1rem] py-2 text-white text-[1.6rem] font-[700] bg-[#F4738D99] rounded-[5rem] border-[#E397BC] border-w-[0.1rem] inline-block">
+                      {cats[indexXingGe].keys[0]}
+                    </div>
+                  </div>
+                )}
+                {indexXingGe != null && (
+                  <div className="">
+                    <div className="px-[1rem] py-2 text-white text-[1.6rem] font-[700] bg-[#2278C199] rounded-[5rem] border-[#6D9FDA] border-w-[0.1rem] inline-block my-[0.6rem]">
+                      {cats[indexXingGe].keys[1]}
+                    </div>
+                  </div>
+                )}
+                {indexXingGe != null && cats[indexXingGe].keys.length > 2 && (
+                  <div className="">
+                    <div className="px-[1rem] py-2 text-white text-[1.6rem] font-[700] bg-[#4A5F7799] rounded-[5rem] border-[#95A1C1] border-w-[0.1rem] inline-block">
+                      {cats[indexXingGe].keys[2]}
+                    </div>
+                  </div>
+                )}
+                {indexXingGe != null && (
+                  <div className="absolute right-[1.6rem] top-[0rem] bg-[#B4886D] border-[1px] border-[#DBB399] rounded-[5rem] flex justify-center items-center px-[1rem] h-[2.8rem]">
+                    <span className="text-white text-[1.2rem] font-[700] text-center leading-[1]">
+                      {cats[indexXingGe].type}
+                    </span>
+                  </div>
+                )}
+                <div className="absolute left-[0] bottom-[-1rem]  w-full flex justify-center">
+                  <img
+                    src="/img/bg_cat.min.png"
+                    className="w-[auto] h-[25rem]"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className=" w-full relative bg-white rounded-bl-[3rem] rounded-br-[3rem]">
+                <div className="absolute top-[-2rem] left-0 w-full flex justify-center items-center">
+                  <div className="relative">
+                    <input
+                      readOnly
+                      value={catName}
+                      className="border-[1px] focus:outline-[#E96856]! border-[#E96856] rounded-full bg-white w-[21rem] h-[4rem] text-[#EA8273] text-[2rem] font-[700] px-[2rem] pr-[3.6rem] text-center"
+                      type="text"
+                    />
+                  </div>
+                </div>
+                <div className="text-[#826662] text-[1.4rem] font-[500] flex flex-col justify-center items-start px-[2rem] text-left">
+                  {/* They have a natural aura, prefer their <br /> space, but once they
+              trust you, they’ll <br /> quietly stay by your side for the long{" "}
+              <br /> run. */}
+                  {indexXingGe != null && (
+                    <div className="mt-[1rem] text-[#826662] text-[1.3rem] font-[700] flex justify-center items-center text-center pt-[2rem] pb-[3rem]">
+                      {cats[indexXingGe].desc}
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <DialogTrigger>
-          <div className="absolute top-[-0.4rem] right-[-1.6rem] flex  justify-center items-center cursor-pointer select-none focus-visible:outline-none">
+          <div className="absolute top-[-0.6rem] right-[1rem] flex  justify-center items-center cursor-pointer select-none focus-visible:outline-none">
             <img
               className="w-[4.4rem] h-[4.4rem]"
               src="/img/close2.min.png"
