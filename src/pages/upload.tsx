562d17b18127d0971e4ccb72c95aa77dd3a8ce8b
pop <906792895@qq.com>
2025-07-25 12:18:20 +0800

    fix: upload toast

--- src/pages/upload.tsx
+++ src/pages/upload.tsx
@@ -12,6 +12,7 @@ const UploadView = () => {
   const router = useRouter();
   const { userData } = useFetchUser();
   const { t } = useTranslation();
+  const [isLoading, setisLoading] = useState(false);
 
   const handleImageChange = (event: any) => {
     const file = event.target.files[0];
@@ -29,6 +30,7 @@ const UploadView = () => {
     const formDataToken = new FormData();
     formDataToken.append("file", file);
     try {
+      setisLoading(true);
       const result: any = await request({
         url: "/api/user/avatar",
         method: "post",
@@ -44,6 +46,7 @@ const UploadView = () => {
     } catch (error) {
       console.log(error);
     }
+    setisLoading(false);
   };
 
   return (
@@ -89,9 +92,7 @@ const UploadView = () => {
               />
               <div className="h-[0.6rem]"></div>
               <div className="text-white text-[1.4rem] font-[500]">
-                
                 {t("chat.Upload a new avatar")}
-
               </div>
             </div>
           </label>
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,12 +1,8 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
-import { get } from "lodash-es"
-import {
-    jwtHelper,
-    JWT_HEADER_KEY,
-} from './jwt';
-import Router from 'next/router'; // 注意不是 useRouter
-
-
+import { get } from "lodash-es";
+import { jwtHelper, JWT_HEADER_KEY } from "./jwt";
+import Router from "next/router"; // 注意不是 useRouter
+import { toast } from "react-toastify";
 
 // export const instance = axios.create({
 //     timeout: 60000,
@@ -30,108 +26,112 @@ import Router from 'next/router'; // 注意不是 useRouter
 
 /** 创建请求实例 */
 function createService() {
-    // 创建一个 axios 实例
-    const service = axios.create()
-    // 请求拦截
-    service.interceptors.request.use(
-        (config) => config,
-        // 发送失败
-        (error) => Promise.reject(error)
-    )
-    // 响应拦截（可根据具体业务作出相应的调整）
-    service.interceptors.response.use(
-        (response) => {
-            // apiData 是 api 返回的数据
-            // const apiData = response.data as any
-            // 这个 code 是和后端约定的业务 code
-            // const code = apiData.code
-            // 如果没有 code, 代表这不是项目后端开发的 api
-            // if (code === undefined) {
-            //   ElMessage.error("非本系统的接口")
-            //   return Promise.reject(new Error("非本系统的接口"))
-            // } else {
-            //   switch (code) {
-            //     case 0:
-            //       // code === 0 代表没有错误
-            //       return apiData
-            //     case 20000:
-            //       // code === 20000 代表没有错误
-            //       return apiData
-            //     default:
-            //       // 不是正确的 code
-            //       ElMessage.error(apiData.msg || "Error")
-            //       return Promise.reject(new Error("Error"))
-            //   }
-            // }
-            return response;
-        },
-        (error) => {
-            // status 是 HTTP 状态码
-            const status = get(error, "response.status")
-            switch (status) {
-                case 400:
-                    error.message = "Request Error"
-                    break
-                case 401:
-                    error.message = "Not authorized, please log in"
-                    jwtHelper.clearToken()
-                    Router.replace('/login')
-                    break
-                case 403:
-                    // token 过期时，直接退出登录并强制刷新页面（会重定向到登录页）
-                    // useUserStoreHook().logout()
-                    // location.reload()
-                    break
-                case 404:
-                    error.message = "Request address error"
-                    break
-                case 408:
-                    error.message = "Request timeout"
-                    break
-                case 500:
-                    error.message = "Internal server error"
-                    break
-                case 501:
-                    error.message = "Service not implemented"
-                    break
-                case 502:
-                    error.message = "Gateway Error"
-                    break
-                case 503:
-                    error.message = "Service Unavailable"
-                    break
-                case 504:
-                    error.message = "Gateway timeout"
-                    break
-                case 505:
-                    error.message = "Unsupported HTTP version"
-                    break
-                default:
-                    break
-            }
-            console.error(error.message)
-            return Promise.reject(error)
-        }
-    )
-    return service
+  // 创建一个 axios 实例
+  const service = axios.create();
+  // 请求拦截
+  service.interceptors.request.use(
+    (config) => config,
+    // 发送失败
+    (error) => Promise.reject(error)
+  );
+  // 响应拦截（可根据具体业务作出相应的调整）
+  service.interceptors.response.use(
+    (response) => {
+      // apiData 是 api 返回的数据
+      // const apiData = response.data as any
+      // 这个 code 是和后端约定的业务 code
+      // const code = apiData.code
+      // 如果没有 code, 代表这不是项目后端开发的 api
+      // if (code === undefined) {
+      //   ElMessage.error("非本系统的接口")
+      //   return Promise.reject(new Error("非本系统的接口"))
+      // } else {
+      //   switch (code) {
+      //     case 0:
+      //       // code === 0 代表没有错误
+      //       return apiData
+      //     case 20000:
+      //       // code === 20000 代表没有错误
+      //       return apiData
+      //     default:
+      //       // 不是正确的 code
+      //       ElMessage.error(apiData.msg || "Error")
+      //       return Promise.reject(new Error("Error"))
+      //   }
+      // }
+      return response;
+    },
+    (error) => {
+      // status 是 HTTP 状态码
+      const status = get(error, "response.status");
+      switch (status) {
+        case 400:
+          error.message = "Request Error";
+          break;
+        case 401:
+          error.message = "Not authorized, please log in";
+          jwtHelper.clearToken();
+          Router.replace("/login");
+          break;
+        case 403:
+          // token 过期时，直接退出登录并强制刷新页面（会重定向到登录页）
+          // useUserStoreHook().logout()
+          // location.reload()
+          break;
+        case 404:
+          error.message = "Request address error";
+          break;
+        case 413:
+          error.message = "Request Entity Too Large";
+          toast.error("Request Entity Too Large");
+          break;
+        case 408:
+          error.message = "Request timeout";
+          break;
+        case 500:
+          error.message = "Internal server error";
+          break;
+        case 501:
+          error.message = "Service not implemented";
+          break;
+        case 502:
+          error.message = "Gateway Error";
+          break;
+        case 503:
+          error.message = "Service Unavailable";
+          break;
+        case 504:
+          error.message = "Gateway timeout";
+          break;
+        case 505:
+          error.message = "Unsupported HTTP version";
+          break;
+        default:
+          break;
+      }
+      console.error(`res ${error.message}`);
+      return Promise.reject(error);
+    }
+  );
+  return service;
 }
 
 /** 创建请求方法 */
 function createRequestFunction(service: AxiosInstance) {
-    return function (config: AxiosRequestConfig) {
-        const configDefault = {
-            headers: {
-                // 携带 token
-                [JWT_HEADER_KEY]: "Bearer " + jwtHelper.getToken(),
-            },
-            timeout: 300000,
-            data: {},
-        }
-        return service(Object.assign(configDefault, config))
-    }
+  return function (config: AxiosRequestConfig) {
+    const configDefault = {
+      headers: {
+        // 携带 token
+        [JWT_HEADER_KEY]: "Bearer " + jwtHelper.getToken(),
+      },
+      timeout: 300000,
+      data: {},
+    };
+    return service(Object.assign(configDefault, config));
+  };
 }
 
 /** 用于网络请求的实例 */
-export const service = createService()
+export const service = createService();
 /** 用于网络请求的方法 */
-export const request = createRequestFunction(service)
+export const request = createRequestFunction(service);
