427674307faed0eece21c1794020658ac037e241
yf_pop <yf_pop@gmail.com>
2025-08-25 23:20:55 +0800

    fix: tabn out

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -57,6 +57,17 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     })();
   }, []);
 
+  useEffect(() => {
+    (async () => {
+      window.addEventListener("storage", function (event) {
+        console.log(`${event.key} ${event.newValue}`);
+        if (event.key === "tabn" && event.newValue === "tabnout") {
+          this.location.reload();
+        }
+      });
+    })();
+  }, []);
+
   return (
     <>
       <Head>
